28a4bf9a79e8997baa59783b81793f0fba1c9774
Geoff R. McLane <ubuntu@geoffair.info>
2015-03-19 12:23:08 +0100

    save output to bldlog-1.txt

--- build/build-me.sh
+++ build/build-me.sh
@@ -4,17 +4,24 @@
 BN=`basename $0`
 TMPINST="$HOME/projects/install/test-zlib"
 
-echo "$BN: building test-zlib project..."
+TMPLOG="bldlog-1.txt"
 
-cmake .. -DCMAKE_INSTALL_PREFIX=$TMPINST
+echo "$BN: building test-zlib project... output to $TMPLOG"
+echo "$BN: building test-zlib project..." > $TMPLOG
+
+echo "$BN:Doing: cmake .. -DCMAKE_INSTALL_PREFIX=$TMPINST"
+echo "$BN:Doing: cmake .. -DCMAKE_INSTALL_PREFIX=$TMPINST" >> $TMPLOG
+cmake .. -DCMAKE_INSTALL_PREFIX=$TMPINST >> $TMPLOG 2>&1
 if [ ! "$?" = "0" ]; then
-    echo "$BN: cmake config, generation error!"
+    echo "$BN: cmake config, generation error! see $TMPLOG"
     exit 1
 fi
 
-make
+echo "$BN: Doing: 'make'"
+echo "$BN: Doing: 'make'" >> $TMPLOG
+make >> $TMPLOG 2>&1
 if [ ! "$?" = "0" ]; then
-    echo "$BN: make error!"
+    echo "$BN: make error! see $TMPLOG"
     exit 1
 fi
 
