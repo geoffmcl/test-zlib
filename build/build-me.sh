2847c402b30058f41efd077901caaf0cb8c2ee30
Geoff R. McLane <ubuntu@geoffair.info>
2015-03-19 20:08:47 +0100

    add VERBOSE and DEBUG options

--- build/build-me.sh
+++ build/build-me.sh
@@ -3,15 +3,34 @@
 #
 BN=`basename $0`
 TMPINST="$HOME/projects/install/test-zlib"
-
+VERBOSE=0
+DBGSYMS=0
+TMPOPTS="-DCMAKE_INSTALL_PREFIX=$TMPINST"
 TMPLOG="bldlog-1.txt"
+
+for arg in $@; do
+      case $arg in
+         VERBOSE) VERBOSE=1
+            TMPOPTS="$TMPOPTS -DCMAKE_VERBOSE_MAKEFILE=TRUE"
+            ;;
+         DEBUG) DBGSYMS=1
+            TMPOTPS="$TMPOPTS -DCMAKE_BUILD_TYPE=Debug -DENABLE_DEBUG_SYMBOLS:BOOL=TRUE"
+            ;;
+         *)
+            echo "$BN: ERROR: Invalid argument [$arg]"
+            echo "$BN: Only VERBOSE and DEBUG allowed"
+            exit 1
+            ;;
+      esac
+done
+
 
 echo "$BN: building test-zlib project... output to $TMPLOG"
 echo "$BN: building test-zlib project..." > $TMPLOG
 
-echo "$BN:Doing: cmake .. -DCMAKE_INSTALL_PREFIX=$TMPINST"
-echo "$BN:Doing: cmake .. -DCMAKE_INSTALL_PREFIX=$TMPINST" >> $TMPLOG
-cmake .. -DCMAKE_INSTALL_PREFIX=$TMPINST >> $TMPLOG 2>&1
+echo "$BN:Doing: cmake .. $TMPOPTS"
+echo "$BN:Doing: cmake .. $TMPOPTS" >> $TMPLOG
+cmake .. $TMPOPTS >> $TMPLOG 2>&1
 if [ ! "$?" = "0" ]; then
     echo "$BN: cmake config, generation error! see $TMPLOG"
     exit 1
